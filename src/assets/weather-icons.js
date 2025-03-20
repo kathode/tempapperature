45f1ac17d8ff0ac71e1176f3e5d869a2f8e4b4b9
Kathode <work.qiyan@gmail.com>
2025-03-20 13:39:48 +1100

    Add weather-icons import export javascript file

---
+++ src/assets/weather-icons.js
@@ -0,0 +1,37 @@
+import clearDay from "./clear-day.png";
+import clearNight from "./clear-night.png";
+import cloudy from "./cloudy.png";
+import fog from "./fog.png";
+import haze from "./haze.png";
+import heavyRain from "./heavy-rain.png";
+import heavySnow from "./heavy-snow.png";
+import partlyCloudyDay from "./partly-cloudy-day.png";
+import partlyCloudyNight from "./partly-cloudy-night.png";
+import rain from "./rain.png";
+import showersDay from "./showers-day.png";
+import showersNight from "./showers-night.png";
+import snowShowers from "./snow-showers.png";
+import snow from "./snow.png";
+import sunrise from "./sunrise.png";
+import thunderRain from "./thunder-rain.png";
+import wind from "./wind.png";
+
+export const weatherIcons = {
+  "clear-day": clearDay,
+  "clear-night": clearNight,
+  cloudy: cloudy,
+  fog: fog,
+  haze: haze,
+  "heavy-rain": heavyRain,
+  "heavy-snow": heavySnow,
+  "partly-cloudy-day": partlyCloudyDay,
+  "partly-cloudy-night": partlyCloudyNight,
+  rain: rain,
+  "showers-day": showersDay,
+  "showers-night": showersNight,
+  "snow-showers": snowShowers,
+  snow: snow,
+  sunrise: sunrise,
+  "thunder-rain": thunderRain,
+  wind: wind,
+};
