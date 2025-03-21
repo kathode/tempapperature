e69aa30723583eea18bdc1935631ae05fa57c420
Kathode <work.qiyan@gmail.com>
2025-03-21 15:55:24 +1100

    Move day forecast dom render into a function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { format, isToday } from "date-fns";
-import { createElement, get } from "./helpers";
+import { format, getHours, isToday } from "date-fns";
+import { createElement, get, getHourFromTime } from "./helpers";
 import { weatherIcons } from "./assets/weather-icons";
 import "./styles.css";
 
@@ -28,6 +28,57 @@ const getRange = (days) => {
   return { min, max };
 };
 
+const daysForecast = (results) => {
+  const text = document.querySelector(".results");
+  const container = createElement("div", { className: "container" });
+  const range = getRange(results.days);
+
+  container.append(createElement("div", { textContent: "15 day forecast", className: "view-header" }));
+  for (const day of results.days) {
+    const column = createElement("div", { className: "column" });
+    const tempRange = createElement("div", { className: "row range" });
+    const minWidth = ((day.tempmin - range.min) / (range.max - range.min)) * 150;
+    const maxWidth = ((range.max - day.tempmax) / (range.max - range.min)) * 150;
+
+    tempRange.style.setProperty("--before-width", `${Math.round(minWidth)}px`);
+    tempRange.style.setProperty("--after-width", `${Math.round(maxWidth)}px`);
+
+    if (isToday(day.datetime)) {
+      const currentTemp = createElement("div", { className: "current-temp" });
+      currentTemp.style.setProperty("--current-temp", `${Math.round(((day.temp - range.min) / (range.max - range.min)) * 150) - 4}px`);
+      tempRange.append(currentTemp);
+
+      column.append(
+        createElement("div", { className: "row", textContent: `Today` }),
+        createElement("img", { className: "row", src: weatherIcons[day.icon], alt: day.icon }),
+        createElement(
+          "div",
+          { className: "weather-range" },
+          createElement("div", { className: "row center min-temp", textContent: `${Math.round(day.tempmin)}°` }),
+          tempRange,
+          createElement("div", { className: "row center max-temp", textContent: `${Math.round(day.tempmax)}°` })
+        )
+      );
+      container.append(column);
+    } else {
+      column.append(
+        createElement("div", { className: "row", textContent: format(day.datetime, "EEE") }),
+        createElement("img", { className: "row", src: weatherIcons[day.icon], alt: day.icon }),
+        createElement(
+          "div",
+          { className: "weather-range" },
+          createElement("div", { className: "row center min-temp", textContent: `${Math.round(day.tempmin)}°` }),
+          tempRange,
+          createElement("div", { className: "row center max-temp", textContent: `${Math.round(day.tempmax)}°` })
+        )
+      );
+      container.append(column);
+    }
+  }
+
+  text.append(container);
+};
+
 const form = document.querySelector("form");
 
 form.addEventListener("submit", async (event) => {
@@ -39,45 +90,13 @@ form.addEventListener("submit", async (event) => {
   try {
     text.textContent = "";
     text.append(createElement("div", { className: "loader" }));
-
     results = await getWeather(location);
-    const range = getRange(results.days);
     text.textContent = "";
 
-    for (const day of results.days) {
-      const column = createElement("div", { className: "column" });
-      const tempRange = createElement("div", { className: "row range" });
-      const minWidth = ((day.tempmin - range.min) / (range.max - range.min)) * 150;
-      const maxWidth = ((range.max - day.tempmax) / (range.max - range.min)) * 150;
-
-      tempRange.style.setProperty("--before-width", `${Math.round(minWidth)}px`);
-      tempRange.style.setProperty("--after-width", `${Math.round(maxWidth)}px`);
-
-      if (isToday(day.datetime)) {
-        const currentTemp = createElement("div", { className: "current-temp" });
-        currentTemp.style.setProperty("--current-temp", `${Math.round(((day.temp - range.min) / (range.max - range.min)) * 150) - 4}px`);
-        tempRange.append(currentTemp);
-        column.append(
-          createElement("div", { className: "row", textContent: `Today` }),
-          createElement("img", { className: "row", src: weatherIcons[day.icon], alt: day.icon }),
-          createElement("div", { className: "row center min-temp", textContent: `${Math.round(day.tempmin)}°` }),
-          tempRange,
-          createElement("div", { className: "row center max-temp", textContent: `${Math.round(day.tempmax)}°` })
-        );
-        text.append(column);
-      } else {
-        column.append(
-          createElement("div", { className: "row", textContent: format(day.datetime, "EEE") }),
-          createElement("img", { className: "row", src: weatherIcons[day.icon], alt: day.icon }),
-          createElement("div", { className: "row center min-temp", textContent: `${Math.round(day.tempmin)}°` }),
-          tempRange,
-          createElement("div", { className: "row center max-temp", textContent: `${Math.round(day.tempmax)}°` })
-        );
-        text.append(column);
-      }
-    }
+    daysForecast(results);
   } catch (e) {
     text.textContent = "";
     text.textContent = e;
+    console.log(e);
   }
 });
