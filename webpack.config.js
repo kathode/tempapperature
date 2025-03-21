94d9244ec340187707967940ffd5f3799151f675
Kathode <work.qiyan@gmail.com>
2025-03-21 17:04:19 +1100

    Configure webpack to display favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -19,6 +20,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "src/assets/favicon.png", to: "favicon.png" }],
     }),
   ],
 
